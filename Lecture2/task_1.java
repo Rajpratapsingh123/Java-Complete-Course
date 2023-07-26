fd41cf78de7b331b6fbcd4d8feb2e14bd1f2e269
Rajpratapsingh123 <rajpratapsingh471@gmail.com>
2023-07-27 00:43:15 +0530

    Task 1

---
+++ Lecture2/task_1.java
@@ -0,0 +1,17 @@
+// qs. Print the Pattern
+
+// *
+// ** 
+// *** 
+// ****
+
+package Lecture2;
+
+public class task_1 {
+    public static void main(String[] args) {
+       
+        System.out.print("*\n**\n***\n****");
+        
+    }
+    
+}
