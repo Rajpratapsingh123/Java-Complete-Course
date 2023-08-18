01d7ec084613104e26e1316b048dcb6501177b70
Rajpratapsingh123 <rajpratapsingh471@gmail.com>
2023-08-18 22:34:45 +0530

    conditionals

---
+++ Lecture_3/conditionals.java
@@ -0,0 +1,18 @@
+package Lecture_3;
+import java.util.*;
+
+
+public class conditionals {
+    public static void main(String[] args) {
+         Scanner sc=new Scanner(System.in);
+         int age=sc.nextInt();
+
+         if(age>18){
+            System.out.println("Adult");
+         }else{
+            System.out.println("not adult");
+         }
+    }
+   
+    
+}
