be1903c0e693bafccc466c3769f48c72e5c2dfb1
Rajpratapsingh123 <rajpratapsingh471@gmail.com>
2023-08-20 22:53:55 +0530

    Java loop

---
+++ Lecture_4/loops.java
@@ -0,0 +1,9 @@
+package Lecture_4;
+
+public class loops {
+    public static void main(String[] args) {
+        for(int a=0;a<100;a=a+1){
+            System.out.println("hello world");
+        }
+    }
+}
