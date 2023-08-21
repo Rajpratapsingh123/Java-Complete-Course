63536b7a0da5497f0782275a0fbf5fb7d722b912
Rajpratapsingh123 <rajpratapsingh471@gmail.com>
2023-08-21 23:30:16 +0530

    loop task in java

---
+++ Lecture_4/task1.java
@@ -0,0 +1,19 @@
+//  Print the sum of First n natural number
+
+
+
+package Lecture_4;
+import java.util.*;
+public class task1 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner (System.in);
+        int n=sc.nextInt();
+        
+        int sum=0;
+        for(int i=1;i<=n;i++){
+            sum=sum+i;
+        }
+        System.out.println(sum);
+    }
+    
+}
