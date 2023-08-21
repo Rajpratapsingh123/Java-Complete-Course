ae22988f519d67a0fbbf4acc07a1fbed39dfb38f
Rajpratapsingh123 <rajpratapsingh471@gmail.com>
2023-08-21 23:32:11 +0530

    while loop in java

---
+++ Lecture_4/while_loop.java
@@ -0,0 +1,12 @@
+package Lecture_4;
+
+public class while_loop {
+    public static void main(String[] args) {
+        int i=0;
+        while(i<10){
+            System.out.println(i);
+            i++;
+        }
+    }
+    
+}
