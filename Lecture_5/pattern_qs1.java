9005f2bab64d285bc8342d3de02c0ea9a1451716
Rajpratapsingh123 <rajpratapsingh471@gmail.com>
2023-08-22 22:00:48 +0530

    Pattern question 1

---
+++ Lecture_5/pattern_qs1.java
@@ -0,0 +1,16 @@
+// Print solid rectangle Pattern
+
+
+
+package Lecture_5;
+
+public class pattern_qs1 {
+    public static void main(String[] args) {
+        for(int i=1;i<5;i++){
+        for(int j=1;j<6;j++){
+            System.out.print("*");
+        }
+        System.out.println();
+    }
+    }
+}
