aa17e03f666c43554479a79100a33c2cd4f9d19d
Rajpratapsingh123 <rajpratapsingh471@gmail.com>
2023-08-22 21:58:43 +0530

    task 2

---
+++ Lecture_4/task2.java
@@ -0,0 +1,17 @@
+
+// Print the table of a number input by the user 
+
+
+package Lecture_4;
+import java.util.*;
+public class task2 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+
+        for(int i=1;i<=10;i++){
+            System.out.println(i*n);
+        }
+    }
+    
+}
