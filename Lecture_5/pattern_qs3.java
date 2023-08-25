4f06d992986c760b2c1f1f29cc31d3c0d9f77020
Rajpratapsingh123 <rajpratapsingh471@gmail.com>
2023-08-25 22:26:07 +0530

    Pattern 5

---
+++ Lecture_5/pattern_qs3.java
@@ -0,0 +1,18 @@
+package Lecture_5;
+
+public class pattern_qs3 {
+    public static void main(String[] args) {
+        int n=4;
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                            System.out.print("*");
+            }
+            System.out.println();
+
+
+        }
+        
+    }
+    
+}
